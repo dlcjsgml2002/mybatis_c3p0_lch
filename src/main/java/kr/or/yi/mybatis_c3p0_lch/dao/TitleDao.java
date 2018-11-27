59a96e7e2ab2fbaf5c7535f8394e9dd132c4a565
fpvks <rephanoise@naver.com>
2018-11-27 16:50:48 +0900

    MyBatis 기본설정작업

---
+++ src/main/java/kr/or/yi/mybatis_c3p0_lch/dao/TitleDao.java
@@ -0,0 +1,17 @@
+package kr.or.yi.mybatis_c3p0_lch.dao;
+
+import java.util.List;
+
+import kr.or.yi.mybatis_c3p0_lch.dto.Title;
+
+public interface TitleDao {
+	List<Title> selectTitleByAll();
+
+	Title selectTitleByCode(Title title);
+
+	int insertTitle(Title title);
+
+	int deleteTitle(String code);
+
+	int updateTitle(Title title);
+}
